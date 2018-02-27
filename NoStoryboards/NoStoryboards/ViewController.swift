98c3ed5aab36b480cb652cafae3a1dc32c6bbf10
ico3939 <ico3939@g.rit.edu>
2018-02-27 18:52:20 -0500

    Add files via upload

---
+++ NoStoryboards/NoStoryboards/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NoStoryboards
+//
+//  Created by Student on 2/27/18.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(#function)
+        view.backgroundColor = .red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

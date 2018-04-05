a01b6f49580c1fde21196e120a0cc7efa9fbc9f2
Student <student@raj29.istlabs.rit.edu>
2018-04-05 18:49:19 -0400

    Finished GitHub Search

--- GitHubProfile/GitHubProfile/ViewController.swift
+++ GitHubProfile/GitHubProfile/ViewController.swift
@@ -11,14 +11,54 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // guard statements let us avoid Pyramid of doom (nested if statements)
-        guard let githubURL = URL(string:"https://api.github.com/users/ico3939")
+        
+    }
+    
+    func downloadProfilePhoto(url: URL) {
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                print("invalid image request data: \(String(describing: error))")
+                return
+            }
+            
+            let image = UIImage.init(data: data)
+            
+            print(image)
+            // display in UI
+            
+            DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }.resume()
+    }
+    
+    func updateUI(profile: GitHubProfile) {
+        print(profile.name)
+        print(profile.location)
+        print(profile.avatarUrl)
+        self.downloadProfilePhoto(url: profile.avatarUrl!)
+        
+        DispatchQueue.main.async {
+            self.nameLabel.text = profile.name
+            self.locationLabel.text = profile.location
+        }
+        
+    }
+    
+    func searchForUser(username: String) {
+        guard let githubURL = URL(string:"https://api.github.com/users/\(username)")
             else {
-            print("Invalid URL")
-            return
+                print("Invalid URL")
+                return
         }
         print(githubURL)
         
@@ -35,11 +75,7 @@ class ViewController: UIViewController {
                 let decoder = JSONDecoder()
                 let profile = try decoder.decode(GitHubProfile.self, from: data)
                 
-                print(profile.name)
-                print(profile.location)
-                print(profile.avatarUrl)
-                
-                self.downloadProfilePhoto(url: profile.avatarUrl!)
+                self.updateUI(profile: profile)
                 
             }
             catch {
@@ -50,23 +86,8 @@ class ViewController: UIViewController {
         
     }
     
-    func downloadProfilePhoto(url: URL) {
-        
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else {
-                print("invalid image request data: \(error)")
-                return
-            }
-            
-            let image = UIImage.init(data: data)
-            
-            print(image)
-            // display in UI
-            
-            DispatchQueue.main.async {
-                self.imageView.image = image
-            }
-        }.resume()
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        searchForUser(username: usernameTextField.text!)
     }
 
 
